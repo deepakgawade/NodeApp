51fa7fcbc1616156d2b113958a631697e733fbe4
deepakgawade <deepakgawade22@gmail.com>
2022-02-03 11:02:08 +0530

    commit 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,10 @@
-const validator = require('validator') 
-const chakl = require('chalk')
-const getnote = require('./notes.js')
+const validator = require("validator");
+const chakl = require("chalk");
+const getnote = require("./notes.js");
+const yargs = require("yargs");
 
 // const fs = require('fs')
-// fs.appendFileSync('notes.txt', 'I live in Goa!!!') 
+// fs.appendFileSync('notes.txt', 'I live in Goa!!!')
 // const msg=getnote();
 // console.log(msg);
 
@@ -12,11 +13,40 @@ const getnote = require('./notes.js')
 // console.log(validator.isEmail('adghsggail.com'));
 // console.log(chakl.green("Sucess!"));
 
-const comand = process.argv[2];
+// const comand = process.argv[2];
 
-if(comand ==='add'){
-    console.log('Adding Notes')
-}
+// if (comand === "add") {
+//   console.log("Adding Notes");
+// }
 
-//console.log(process.argv)
+yargs.command({
+  command: "add",
+  describe: "add a new note",
+  handler: function () {
+    console.log("Adding new note");
+  },
+});
 
+yargs.command({
+  command: "remove",
+  describe: "reove a new note",
+  handler: function () {
+    console.log("removing note");
+  },
+});
+yargs.command({
+  command: "read",
+  describe: "read a new note",
+  handler: function () {
+    console.log("reading new note");
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "list a new note",
+  handler: function () {
+    console.log("listing new note");
+  },
+});
+console.log(yargs.argv);
+//add, remove, read, list
