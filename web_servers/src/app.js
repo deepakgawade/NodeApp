14b637eb4041f5360c2282cd99947f2ab2f96dec
deepakgawade <deepakgawade22@gmail.com>
2022-03-15 17:47:28 +0530

    commit

--- web_servers/src/app.js
+++ web_servers/src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+console.log(__dirname);
+console.log(path.join(__dirname, "../public"));
+const filepath = path.join(__dirname, "../public");
+app.use(express.static(filepath));
 
 app.get("", (req, res) => {
   res.send("Hello Express");
@@ -14,6 +19,6 @@ app.get("/Home", (req, res) => {
   res.send({ name: "Deepak", age: 25 });
 });
 
-app.listen(3000, (req, res) => {
+app.listen(4000, (req, res) => {
   console.log("Server is up and running");
 });
